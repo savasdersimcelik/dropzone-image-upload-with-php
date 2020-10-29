191422c30434fff46d4328a32a26656b53feb759
Savaş Dersim Çelik <33200780+savasdersimcelik@users.noreply.github.com>
2020-10-29 21:33:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dropzone-image-upload-with-php
+ PHP ile Dropzone ile Resim Upload Sistemi
---
+++ upload.php
@@ -0,0 +1,13 @@
+<?php
+$uploaddir = './uploads/';
+$uploadfile = $uploaddir . basename($_FILES['file']['name']);
+
+if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
+    echo "File is valid, and was successfully uploaded.\n";
+} else {
+    echo "Possible file upload attack!\n";
+}
+
+echo $uploadfile;
+
+?>
